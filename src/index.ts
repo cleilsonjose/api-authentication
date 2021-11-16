2b73c93325172f5fa7ce9eaa3050501ae6a3064e
cleilson <cleilsonrock@gmail.com>
2021-11-16 16:07:48 -0300

    post/put/delete

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express, {Request, Response, NextFunction} from 'express'
+import userRouter from './routes/users.route'
 
 const app = express()
+// Configurações da aplicação
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// Configurações de rotas
+app.use(userRouter)
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({foo:'batata'})
 })
-
+// Inicialização do servidor
 app.listen(3000, ()=>{
   console.log('listening on port 3000')
 })
