7b63de8ec8109c88dfa62966063d40d2de451a73
cleilson <cleilsonrock@gmail.com>
2021-11-16 16:20:00 -0300

    statusRouter

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-import express, {Request, Response, NextFunction} from 'express'
-import userRouter from './routes/users.route'
+import express, { Request, Response, NextFunction } from "express";
+import statusRoute from "./routes/status.route";
+import userRouter from "./routes/users.route";
 
-const app = express()
+const app = express();
 // Configurações da aplicação
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Configurações de rotas
-app.use(userRouter)
+app.use(statusRoute);
+app.use(userRouter);
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({foo:'batata'})
-})
 // Inicialização do servidor
-app.listen(3000, ()=>{
-  console.log('listening on port 3000')
-})
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
