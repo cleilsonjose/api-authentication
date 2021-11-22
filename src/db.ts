4b9b870e0cf670e9d4e5c0a4d3fd73631d30d46b
Cleilson José <39439932+cleilsonjose@users.noreply.github.com>
2021-11-22 07:11:34 -0300

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import {Pool} from 'pg';
 
-const connectionString = 'postgres://zipgybij:meK_JMkaf7Kgz8Hbo8vtbv_2Ay2A3cTx@fanny.db.elephantsql.com/zipgybij';
+const connectionString = '';
 const db = new Pool({connectionString});
 
 export default db;
